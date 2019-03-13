584bbe8d6992a93cfda8d4b4e3326b608f481f3f
Shoaib <ahmed.shoaib1212@gmail.com>
2019-03-13 20:02:30 -0100

    handles exceptions while doing frame counts

--- pre_process.py
+++ pre_process.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import cv2
 import pathlib
 import subprocess
@@ -14,7 +15,7 @@ classes = []
 for root,dirs,file in os.walk("./kth"):
     for dir_name in dirs:
         classes.append(dir_name)
-
+print(classes)
 
 
 dirName = 'processed'
@@ -23,12 +24,22 @@ if not os.path.exists(dirName):
     print("Pre-processing completed")
     for curr_class in classes:
         os.mkdir(dirName+"/"+curr_class)
-        curr_dir = pathlib.Path('./'+curr_class)
-        for curr_file in currentDirectory.glob(currentPattern):
-            success = 1
+        curr_dir = pathlib.Path('./kth/'+curr_class)
+        for curr_file in curr_dir.glob(curr_pat):
             count = 0
-            vidObj = cv2.VideoCapture(curr_file)
-            frames = int(subprocess.check_output("ffmpeg -i "+curr_file+" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' | cut -f 3 -d ' '", shell=True))
+            vidObj = cv2.VideoCapture(str(curr_file))
+            try :
+                frames = int(subprocess.check_output("ffmpeg -i "+str(curr_file)+" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' | cut -f 3 -d ' '", shell=True))
+            except ValueError:
+                continue
+            else :
+                for x in range(network_length):
+                    vidObj.set(2,(x+))
+
+                #print(str(curr_file)," : ",frames)
+                #vidObj.release()
+            
+            '''
             
 
             #vidObj.release()
@@ -38,6 +49,7 @@ if not os.path.exists(dirName):
                 cv2.imwrite("frame%d.jpg" % count, image) 
   
                 count += 1
+            '''
 
 
 
