d2a4371277e3ca4472ebb7d78c0b5b9d40f6a70a
Shoaib <ahmed.shoaib1212@gmail.com>
2019-03-13 18:58:11 -0100

    adds frames counter

--- pre_process.py
+++ pre_process.py
@@ -12,8 +12,8 @@ curr_pat = "*.avi"
 
 classes = []
 for root,dirs,file in os.walk("./kth"):
-	for dir_name in dirs:
-		classes.append(dir_name)
+    for dir_name in dirs:
+        classes.append(dir_name)
 
 
 
@@ -22,21 +22,22 @@ if not os.path.exists(dirName):
     os.mkdir(dirName)
     print("Pre-processing completed")
     for curr_class in classes:
-    	os.mkdir(dirName+"/"+curr_class)
-    	curr_dir = pathlib.Path('./'+curr_class)
-    	for curr_file in currentDirectory.glob(currentPattern):
-    		success = 1
-    		count = 0
-    		vidObj = cv2.VideoCapture(curr_file)
-    		frames = os.system("ffmpeg -i "+curr_file+" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' | cut -f 3 -d ' '")
+        os.mkdir(dirName+"/"+curr_class)
+        curr_dir = pathlib.Path('./'+curr_class)
+        for curr_file in currentDirectory.glob(currentPattern):
+            success = 1
+            count = 0
+            vidObj = cv2.VideoCapture(curr_file)
+            frames = int(subprocess.check_output("ffmpeg -i "+curr_file+" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' | cut -f 3 -d ' '", shell=True))
+            
 
-    		#vidObj.release()
-    		for x in range(network_length):
-	        	
-	        	success, image = vidObj.read() 
-        		cv2.imwrite("frame%d.jpg" % count, image) 
+            #vidObj.release()
+            for x in range(network_length):
+                
+                success, image = vidObj.read() 
+                cv2.imwrite("frame%d.jpg" % count, image) 
   
-        		count += 1
+                count += 1
 
 
 
