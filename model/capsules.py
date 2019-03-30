460a12d5b1a960e86eda40484d759e49c30c62cf
ahmedshoaib <shoaib@localhost.localdomain>
2019-03-30 11:08:55 +0530

    Adjusts kernel sizes

--- model/capsules.py
+++ model/capsules.py
@@ -320,7 +320,7 @@ class CapsNet(nn.Module):
     def __init__(self, A=32, B=32, C=32, D=32, E=6, K=3, P=4, iters=3):
         super(CapsNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=A,
-                               kernel_size=4, stride=3, padding=1) 
+                               kernel_size=4, stride=2, padding=0) 
         self.bn1 = nn.BatchNorm2d(num_features=A, eps=0.001,
                                  momentum=0.1, affine=True)
         self.relu1 = nn.ReLU(inplace=False)
@@ -331,6 +331,7 @@ class CapsNet(nn.Module):
                                         coor_add=True, w_shared=True) 
         
     def forward(self, x):
+        #print(x.size())
         x = self.conv1(x)
         #print("After Conv :")
         #print(x.size())
